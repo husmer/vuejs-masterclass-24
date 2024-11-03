d121e6e5641869aa880c4f87dcc025d84e2cb29a
ovilu <vilu.oliver@gmail.com>
2024-11-03 19:46:07 +0200

    feat: break

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import { h } from 'vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: () => import('@/views/ProjectsView.vue'),
+    },
+    {
+      // /projects/1
+      path: '/projects/:id',
+      name: 'single-project',
+      component: () => import('@/views/SingleProjectView.vue'),
+    },
+    {
+      // error 404
+      path: '/:catchAll(.*)*',
+      name: 'NotFound',
+      component: h('p', { style: { color: 'red' } }, '404 Page not found'),
+    },
+  ],
 })
 
 export default router
